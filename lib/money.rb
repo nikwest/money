6ed676b168012aa0105f1d43b48034387924d445
Dominik Westner <westner@logicunited.com>
2009-02-07 16:16:05 +0100

    * adapted upstream changes

--- lib/money.rb
+++ lib/money.rb
@@ -1,22 +1,23 @@
 require 'support/cattr_accessor'
 require 'money/bank/no_exchange_bank'
 require 'money/bank/variable_exchange_bank'
+require 'money/bank/currency_exchange_bank'
 require 'money/core_extensions'
 
 # === Usage with ActiveRecord
 # 
 # Use the compose_of helper to let active record deal with embedding the money
-# object in your models. The following example requires a cents and a currency field.
+# object in your models. The following example requires a amount and a currency field.
 # 
 #   class ProductUnit < ActiveRecord::Base
 #     belongs_to :product
-#     composed_of :price, :class_name => "Money", :mapping => [ %w(cents cents), %w(currency currency) ]
+#     composed_of :price, :class_name => "Money", :mapping => [ %w(amount amount), %w(currency currency) ]
 # 
 #     private        
-#       validate :cents_not_zero
+#       validate :amount_not_zero
 #     
-#       def cents_not_zero
-#         errors.add("cents", "cannot be zero or less") unless cents > 0
+#       def amount_not_zero
+#         errors.add("amount", "cannot be zero or less") unless amount > 0
 #       end
 #     
 #       validates_presence_of :sku, :currency
@@ -26,7 +27,7 @@ require 'money/core_extensions'
 class Money
   include Comparable
 
-  attr_reader :cents, :currency, :precision
+  attr_reader :amount, :currency, :precision
 
   class MoneyError < StandardError# :nodoc:
   end
@@ -45,31 +46,39 @@ class Money
   @@bank = NoExchangeBank.new
   cattr_accessor :bank
 
-  @@default_currency = "USD"
+  @@default_currency = "EUR"
   cattr_accessor :default_currency
   
   # String to use when formating zero values
   cattr_accessor :zero
+  
+  @@symbols = {
+    'USD' => '$',
+    'GBP' => '£',
+    'JPY' => '¥',
+    'EUR' => '$',
+    'CHF' => 'SwF'
+  }
 
   # Creates a new money object. 
   #  Money.new(100) 
   # 
   # Alternativly you can use the convinience methods like 
   # Money.ca_dollar and Money.us_dollar 
-  def initialize(cents, currency = default_currency, precision = 2)
-    @cents, @currency, @precision = cents.round, currency, precision
+  def initialize(amount, currency = default_currency, precision = 2)
+    @amount, @currency, @precision = amount.round, currency, precision
   end
 
   # Do two money objects equal? Only works if both objects are of the same currency
   def eql?(other_money)
-    cents == other_money.cents && currency == other_money.currency
+    amount == other_money.amount && currency == other_money.currency
   end
 
   def <=>(other_money)
     if currency == other_money.currency
-      cents <=> other_money.cents
+      amount <=> other_money.amount
     else
-      cents <=> other_money.exchange_to(currency).cents
+      amount <=> other_money.exchange_to(currency).amount
     end
   end
 
@@ -77,38 +86,38 @@ class Money
     other_money = other_money.exchange_to(currency) unless other_money.currency == currency
     
     new_precision = [precision, other_money.precision].max
-    Money.new(to_precision(new_precision).cents + other_money.to_precision(new_precision).cents, currency, new_precision)
+    Money.new(to_precision(new_precision).amount + other_money.to_precision(new_precision).amount, currency, new_precision)
   end
 
   def -(other_money)
     other_money = other_money.exchange_to(currency) unless other_money.currency == currency
     
     new_precision = [precision, other_money.precision].max
-    Money.new(to_precision(new_precision).cents - other_money.to_precision(new_precision).cents, currency, new_precision)
+    Money.new(to_precision(new_precision).amount - other_money.to_precision(new_precision).amount, currency, new_precision)
   end
 
   def -@
-    Money.new(-cents, currency, precision)
+    Money.new(-amount, currency, precision)
   end
 
   # multiply money by fixnum
   def *(fixnum)
-    Money.new(cents * fixnum, currency, precision)
+    Money.new(amount * fixnum, currency, precision)
   end
 
   # divide money by fixnum
   def /(fixnum)
-    Money.new(cents / fixnum, currency, precision)
+    Money.new(amount / fixnum, currency, precision)
   end
   
   # Test if the money amount is zero
   def zero?
-    cents == 0 
+    amount == 0 
   end
 
 
   # Format the price according to several rules
-  # Currently supported are :with_currency, :no_cents and :html
+  # Currently supported are :with_currency, :no_fraction and :html
   #
   # with_currency: 
   #
@@ -117,13 +126,13 @@ class Money
   #  Money.ca_dollar(100).format(:with_currency) => "$1.00 CAD"
   #  Money.us_dollar(85).format(:with_currency) => "$0.85 USD"
   #
-  # no_cents:  
+  # no_fraction:  
   #
-  #  Money.ca_dollar(100).format(:no_cents) => "$1"
-  #  Money.ca_dollar(599).format(:no_cents) => "$5"
+  #  Money.ca_dollar(100).format(:no_fraction) => "$1"
+  #  Money.ca_dollar(599).format(:no_fraction) => "$5"
   #  
-  #  Money.ca_dollar(570).format(:no_cents, :with_currency) => "$5 CAD"
-  #  Money.ca_dollar(39000).format(:no_cents) => "$390"
+  #  Money.ca_dollar(570).format(:no_fraction, :with_currency) => "$5 CAD"
+  #  Money.ca_dollar(39000).format(:no_fraction) => "$390"
   #
   # html:
   #
@@ -133,7 +142,7 @@ class Money
     
     rules = rules.flatten
 
-    formatted = "$" + to_s(rules.include?(:no_cents) ? 0 : 2)
+    formatted = @@symbols[currency] + to_s(rules.include?(:no_fraction) ? 0 : 2)
 
     if rules.include?(:with_currency)
       formatted << " "
@@ -149,12 +158,12 @@ class Money
     if show_precision > 0
       sprintf("%.#{show_precision}f", to_f  )
     else
-      sprintf("%d", cents.to_f / 10 ** (precision - show_precision)  )
+      sprintf("%d", amount.to_f / 10 ** (precision - show_precision)  )
     end
   end
   
   def to_f
-    cents.to_f / 10 ** precision
+    amount.to_f / 10 ** precision
   end
 
   # Recieve the amount of this money object in another currency   
@@ -164,8 +173,8 @@ class Money
   
   def to_precision(new_precision)
     difference = new_precision - precision
-    new_cents = difference > 0 ? cents * 10**difference : (cents.to_f / 10**difference.abs).round
-    Money.new(new_cents, currency, new_precision)
+    new_amount = difference > 0 ? amount * 10**difference : (amount.to_f / 10**difference.abs).round
+    Money.new(new_amount, currency, new_precision)
   end
 
   # Create a new money object with value 0
@@ -202,7 +211,7 @@ class Money
 
   # Recieve a money object with the same amount as the current Money object
   # in euro
-  def as_ca_euro
+  def as_euro
     exchange_to("EUR")
   end  
 
